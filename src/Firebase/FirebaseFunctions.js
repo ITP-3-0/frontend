afc253c56d7cd7df91e0bf73fd60f1efe89d38e2
sanirurajapaksha <sanirurajapaksha456@gmail.com>
2025-03-20 16:14:37 +0530

    Enhance user registration: add username extraction from email and store user data in the database; update NavBar button to redirect to login

--- src/Firebase/FirebaseFunctions.js
+++ src/Firebase/FirebaseFunctions.js
@@ -2,12 +2,28 @@ import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } f
 import { auth } from "./Initialize";
 
 export const register = async (email, password, censusNo) => {
+    const getUsernameFromEmail = (email) => {
+        return email.split("@")[0];
+    };
+
     try {
         createUserWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 const user = userCredential.user;
                 // API call to store user in database
-                // start from here
+                fetch("/api/users", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        _id: user.uid,
+                        username: getUsernameFromEmail(email),
+                        censusNo: censusNo,
+                        email: email,
+                        role: "client",
+                    }),
+                });
             })
             .then(() => {
                 window.location.href = "/portal";
